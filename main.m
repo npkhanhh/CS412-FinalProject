9576ba785ea0679efff36f47f1925c1f5915b475
npkhanh <franknguyen007@gmail.com>
2015-01-13 17:00:05 +0700

    Load data
    Do data nhiều quá, nên tao bỏ bớt rồi, để lại 3 thư mục thôi.
    Code chỉ mới load 1 thư mục.

---
+++ main.m
@@ -0,0 +1,14 @@
+%%load data
+%load folder list
+list = textread('data\list.txt', '%s');
+
+%load first folder
+folderName = list{1, 1};
+files = dir(['data\' folderName '\*.jpg']);
+filesName = {files.name};
+filesNum = numel(filesName);
+data = cell(filesNum, 1);
+for i=1:filesNum
+    data{i} = imread(['data\' folderName '\' filesName{i}]);
+end
+
