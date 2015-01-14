8be59a330df1cf4e4676af168d6c5fcdb9b3c85b
dhuy93 <d.huy93@gmail.com>
2015-01-14 18:50:09 +0700

    Modify main.m: + Add path separator to be compilable on both PC and UNIX system + Use textscan instead of textread;

--- main.m
+++ main.m
@@ -1,16 +1,30 @@
-%%load data
-%load folder list
-list = textread('data\list.txt', '%s');
+%% Load data
+% load folder list
+pathSeparator = '';
+if ispc
+    pathSeparator = '\';
+elseif isunix
+    pathSeparator = '/';
+end
 
-%load first folder
+fileID = fopen(['data' pathSeparator 'list.txt']); % The ID of the file
+
+list = textscan(fileID, '%s');
+list = list{1, 1};
+
+% load first folder
 folderName = list{1, 1};
-files = dir(['data\' folderName '\*.jpg']);
+files = dir(['data' pathSeparator folderName pathSeparator '*.jpg']);
 filesName = {files.name};
 filesNum = numel(filesName);
 data = cell(filesNum, 1);
+
 for i=1:filesNum
-    data{i} = imread(['data\' folderName '\' filesName{i}]);
+    data{i} = imread(['data' pathSeparator folderName pathSeparator filesName{i}]);
 end
+
 
 imshow(data{1});
 h = imrect;
+
+fclose(fileID);
